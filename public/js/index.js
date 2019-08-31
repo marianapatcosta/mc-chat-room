2a0501fdfe1667e8dec9e2780b9438c60c2684aa
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2019-08-31 16:29:49 +0100

    add dropdown with rooms in use

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+// Elements
+const $roomSelection = document.querySelector("#room-selection");
+const room = document.getElementById('room');
+
+// Templates
+const roomSelectionTemplate = document.querySelector("#room-selection-template").innerHTML;
+
+socket.on("chatRooms", (rooms) => {
+    const html = Mustache.render(roomSelectionTemplate, {
+        rooms
+      });
+      $roomSelection.innerHTML = html;
+});
+
+function getSelectedRoom(event) {
+   room.value = event.target.value
+}
